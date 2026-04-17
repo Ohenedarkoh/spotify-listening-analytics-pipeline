86fb3a0d3a04f8c104593d550b300d57e034464c
Ohenedarkoh <ohenedarkohfrank@gmail.com>
2026-04-17 13:33:51 +0000

    feat(dbt): optimize fact model and strengthen warehouse configuration

--- dbt/spotify_analytics/models/marts/fact_listening.sql
+++ dbt/spotify_analytics/models/marts/fact_listening.sql
@@ -1,4 +1,17 @@
--- Fact table at listening event grain
+{{
+  config(
+    materialized='table',
+    partition_by={
+      "field": "played_at_ts",
+      "data_type": "timestamp",
+      "granularity": "day"
+    },
+    cluster_by=["track_id", "artist_id"]
+  )
+}}
+
+-- Fact table at listening event grain.
+-- Partition by day for time-window queries and cluster by common filter/group keys.
 select
   played_at_ts,
   track_id,
